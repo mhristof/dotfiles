c70db7789bca1e38fe6f9631673a28e8bea0a425
Mike Christof <mike.christofilopoulos@bambooloans.com>
2018-03-13 17:23:45 +0000

    added docker container to the kill list, added icons

--- alfred/pkill.py
+++ alfred/pkill.py
@@ -12,10 +12,14 @@
 
 import os
 from sh import ps
+from sh import docker
 import json
+import re
+import requests
+import shutil
 
 
-def items():
+def ps_items():
     """docstring for items"""
     procs = []
     header_skipped = False
@@ -29,16 +33,59 @@ def items():
                 'uid': output[1],
                 'title': ' '.join(output[4:]),
                 'match': ' '.join(output[4:]).replace('/', ' '),
-                'arg': output[1]
+                'arg': 'kill -9 ' + output[1],
+                'icon': {
+                    'path': wget('https://cdn1.macworld.co.uk/cmsdata/features/3608274/Terminalicon2_thumb800.png')
+                }
             }
             procs.append(proc)
     return procs
 
 
+def simple_image_name(name):
+    """ simplify teh docker image name reported in `docker ps` """
+
+    name = re.sub(':[0-9a-fA-F]{40}', '', name)
+    name = re.sub('.*amazonaws.com/', '', name)
+
+    return name
+
+
+def docker_items():
+    ret = []
+    for item in str(docker('ps', '--format', "table {{.ID}} {{.Image}} {{.Names}} {{.Labels}}")).split('\n')[1:]:
+        fields = item.split()
+        if len(fields) == 0:
+            continue
+        ret += [
+            {
+                'uid': fields[0],
+                'arg': 'docker rm -f ' + fields[0],
+                'match': item.replace('/', ' '),
+                'title': ', '.join([simple_image_name(fields[1]), fields[2]]),
+                'icon': {
+                    'path': wget('https://goo.gl/9iZD4B', '/tmp/docker.jpeg')
+                }
+            }
+        ]
+
+    return ret
+
+
+def wget(url, dest=None):
+    """docstring for download_file"""
+    if dest is None:
+        dest = os.path.join('/tmp', os.path.basename(url))
+    if not os.path.exists(dest):
+        r = requests.get(url, stream=True, allow_redirects=True)
+        with open(dest, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+    return dest
+
 def main():
     """docstring for main"""
     print(json.dumps({
-        'items': items()
+        'items': docker_items() + ps_items()
     }, indent=4))
 
 
