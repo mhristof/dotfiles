d9d822d5107598561aa6634df57adc779d5f1921
Mike Christof <mike.christofilopoulos@bambooloans.com>
2018-04-25 21:29:46 +0100

    alfred: added awk with delimiter snippets

--- alfred/snippets/awk.sh
+++ alfred/snippets/awk.sh
@@ -3,16 +3,33 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+rm json/awk* json/fawk*
+
 for f in $(seq 1 9); do
     uuid=$(uuidgen)
     fout="json/awk-$f-[$uuid].json"
     cat << EOF > $fout
 {
   "alfredsnippet" : {
-    "snippet" : "awk '{print $f}'",
+    "snippet" : "awk '{print \$$f}'",
     "uid" : "$uuid",
     "name" : "awk $f",
     "keyword" : "£awk$f"
+  }
+}
+EOF
+done
+
+for f in $(seq 1 9); do
+    uuid=$(uuidgen)
+    fout="json/fawk-$f-[$uuid].json"
+    cat << EOF > $fout
+{
+  "alfredsnippet" : {
+    "snippet" : "awk -F'{cursor}' '{print \$$f}'",
+    "uid" : "$uuid",
+    "name" : "awk $f with delimiter",
+    "keyword" : "£fawk$f"
   }
 }
 EOF
