bbeb1d9e8cd27163aababb6e8d1ba0f8cd3cc8f3
Mike Christof <mhristof@gmail.com>
2018-05-16 15:50:20 +0100

    cleanup

--- alfred/pbpaste-strings.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-
-pbpaste | strings
-
-exit 0
--- alfred/pbpaste-urlencode.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-
-python3 -c "import urllib.parse; print(urllib.parse.quote_plus('$(pbpaste)'))"
-
-exit 0
