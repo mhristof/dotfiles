7eace5c09e655f241f26c26661fc06b1a6025c1d
Mike Christofilopoulos <mhristof@gmail.com>
2020-02-15 16:26:37 +0000

    cleanup

--- remove-ssh-host.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-#
-# remove-ssh-host.sh
-# Copyright (C) 2017 mhristof <mhristof@Mikes-MacBook-Pro-2.local>
-#
-# Distributed under terms of the MIT license.
-#
-
-sed -i.$(date --iso=seconds | cut -d+ -f1) "/$*/d" ~/.ssh/known_hosts
