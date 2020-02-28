a28644fa762b884111d494536eb69c3e095eb397
Mike Christofilopoulos <mike.christofilopoulos@10xbanking.com>
2020-02-28 10:54:37 +0000

    added squash script

---
+++ squash.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+JIRAS="$(git log --pretty=format:%s master.. | grep -oP '[A-Z]{1,}-[0-9]{1,}' | sort -u | paste -s -d ',')"
+
+git reset --soft $(git merge-base master $(git rev-parse --abbrev-ref HEAD))
+git commit -am "$JIRAS: $(git rev-parse --abbrev-ref HEAD)"
+git rebase master
+
+exit 0
