39e2c513dae29dc8a3e34c2407efa305202a859f
Mike Christofilopoulos <mike.christofilopoulos@10xbanking.com>
2020-03-03 00:57:06 +0000

    dont fail if jira is not found

--- squash.sh
+++ squash.sh
@@ -3,10 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-JIRAS="$(git log --pretty=format:%s master.. | grep -oP '[A-Z]{1,}-[0-9]{1,}' | sort -u | paste -s -d ',')"
+JIRAS="$(git log --pretty=format:%s master.. | grep -oP '[A-Z]{1,}-[0-9]{1,}'| sort -u | paste -s -d ','): " || {
+    JIRAS=""
+}
 
 git reset --soft $(git merge-base master $(git rev-parse --abbrev-ref HEAD))
-git commit -am "$JIRAS: $(git rev-parse --abbrev-ref HEAD)"
+git commit -am "${JIRAS}$(git rev-parse --abbrev-ref HEAD)"
 git rebase master
 
 exit 0
