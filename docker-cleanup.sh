a0e9ae3b8aef982c8c20325ea9b5e0d2fc160498
Mike Christof <mhristof@gmail.com>
2021-02-25 13:34:03 +0000

    darwin install gnudiff

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -6,6 +6,7 @@ docker volume ls -qf dangling=true | xargs --no-run-if-empty docker volume rm
 docker network ls | grep "bridge" | awk '/ / { print $1 }' | xargs --no-run-if-empty docker network rm
 docker images --filter "dangling=true" -q --no-trunc | xargs --no-run-if-empty docker rmi
 docker images | grep "none" | awk '/ / { print $3 }' | xargs --no-run-if-empty docker rmi
+docker images -q | xargs --no-run-if-empty docker rmi -f
 docker ps -qa --no-trunc --filter "status=exited" | xargs --no-run-if-empty docker rm
 
 exit 0
