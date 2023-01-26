63ce819be5068c9bb3da9c3a611381fb7f8ddf6d
Mike Christofilopoulos <mike.christofilopoulos@osl.com>
2023-01-26 10:15:38 +0000

    fix(checkov): add rule name in the disable directive

--- checkov-disable-all.sh
+++ checkov-disable-all.sh
@@ -12,18 +12,20 @@ if [[ ! -f check.txt ]]; then
         id=$(jq -r .check_id <<<"$line")
         path=$(jq -r .file_abs_path <<<"$line")
         lineno=$(jq -r .code_block[0][0] <<<"$line")
+        name=$(jq -r .check_name <<<"$line")
 
-        echo "$path $lineno $id"
+        echo "$path,$lineno,$id,$name"
 
     done < <(checkov --framework terraform --external-checks-dir checkov-rules --directory . -o json | jq -rc '.results.failed_checks[]') | tee check.txt
 fi
 
 while read -r line; do
-    file=$(cut -d ' ' -f1 <<<"$line")
-    ln=$(cut -d ' ' -f2 <<<"$line")
-    id=$(cut -d ' ' -f3 <<<"$line")
+    file=$(cut -d ',' -f1 <<<"$line")
+    ln=$(cut -d ',' -f2 <<<"$line")
+    id=$(cut -d ',' -f3 <<<"$line")
+    name=$(cut -d ',' -f4 <<<"$line")
 
-    sed -i "${ln} a #checkov:skip=${id}" $file
+    sed -i "${ln} a #checkov:skip=${id}:${name}" $file
 done < <(sort -r -k1 -k2 -n check.txt)
 
 exit 0
