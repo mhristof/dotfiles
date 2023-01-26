ec22aa839d8805061a2bd2b03fd370fc3816b68d
Mike Christofilopoulos <mike.christofilopoulos@osl.com>
2023-01-26 10:18:13 +0000

    fix(checkov): create tmp files

--- checkov-disable-all.sh
+++ checkov-disable-all.sh
@@ -2,22 +2,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-die() {
-    echo "$*" 1>&2
-    exit 1
-}
+CHECKS=$(mktemp)
 
-if [[ ! -f check.txt ]]; then
-    while read -r line; do
-        id=$(jq -r .check_id <<<"$line")
-        path=$(jq -r .file_abs_path <<<"$line")
-        lineno=$(jq -r .code_block[0][0] <<<"$line")
-        name=$(jq -r .check_name <<<"$line")
+while read -r line; do
+    id=$(jq -r .check_id <<<"$line")
+    path=$(jq -r .file_abs_path <<<"$line")
+    lineno=$(jq -r .code_block[0][0] <<<"$line")
+    name=$(jq -r .check_name <<<"$line")
 
-        echo "$path,$lineno,$id,$name"
+    echo "$path,$lineno,$id,$name"
 
-    done < <(checkov --framework terraform --external-checks-dir checkov-rules --directory . -o json | jq -rc '.results.failed_checks[]') | tee check.txt
-fi
+done < <(checkov --framework terraform --external-checks-dir checkov-rules --directory . -o json | jq -rc '.results.failed_checks[]') | tee "$CHECKS"
 
 while read -r line; do
     file=$(cut -d ',' -f1 <<<"$line")
@@ -25,7 +20,9 @@ while read -r line; do
     id=$(cut -d ',' -f3 <<<"$line")
     name=$(cut -d ',' -f4 <<<"$line")
 
-    sed -i "${ln} a #checkov:skip=${id}:${name}" $file
-done < <(sort -r -k1 -k2 -n check.txt)
+    sed -i "${ln} a #checkov:skip=${id}:${name}" "$file"
+done < <(sort -r -k1 -k2 -n "$CHECKS")
+
+rm -f "$CHECKS"
 
 exit 0
