002bf810092786c3a06b14b36428abdf3e6a9a40
Mike Christofilopoulos <mhristof@gmail.com>
2026-03-11 14:41:07 +0200

    fix: dynamically find nvim socket instead of hardcoded path

--- nvim-edit.py
+++ nvim-edit.py
@@ -10,10 +10,22 @@
 import os
 import sys
 from pathlib import Path
-
 from pynvim import attach
+import glob
 
-nvim = attach("socket", path="/tmp/nvim")
+# Try to get socket from NVIM environment variable first
+socket_path = os.environ.get('NVIM')
+
+# If not set, try to find a running nvim socket
+if not socket_path:
+    sockets = glob.glob('/tmp/nvim*/0')
+    if sockets:
+        socket_path = sockets[0]
+    else:
+        print("Error: No running nvim instance found", file=sys.stderr)
+        sys.exit(1)
+
+nvim = attach("socket", path=socket_path)
 
 file = os.path.abspath(sys.argv[1])
 Path(file).touch()
