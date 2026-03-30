18c897dca421b8de364f412a46124db5f85eb5f1
Mike Christofilopoulos <mhristof@gmail.com>
2026-03-30 14:34:15 +0300

    feat: use uv inline script metadata for pynvim dependency in nvim scripts and add uv to Brewfile

--- nvim-edit.py
+++ nvim-edit.py
@@ -1,39 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.10"
+# dependencies = ["pynvim"]
+# ///
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-#
-
-"""
-
-"""
 
 import os
 import sys
 from pathlib import Path
-from pynvim import attach
 import glob
+import subprocess
 
 # Try to get socket from NVIM environment variable first
 socket_path = os.environ.get('NVIM')
 
-# If not set, try to find a running nvim socket
+# If not set, try to find the most recent nvim socket
 if not socket_path:
-    sockets = glob.glob('/tmp/nvim*/0')
+    # Find all nvim sockets (both /tmp/nvim.*.sock and /tmp/nvim*/0)
+    sockets = glob.glob('/tmp/nvim.*.sock') + glob.glob('/tmp/nvim*/0')
     if sockets:
-        socket_path = sockets[0]
-    else:
-        print("Error: No running nvim instance found", file=sys.stderr)
-        sys.exit(1)
-
-nvim = attach("socket", path=socket_path)
+        # Use the most recently modified socket
+        socket_path = max(sockets, key=os.path.getmtime)
 
 file = os.path.abspath(sys.argv[1])
 Path(file).touch()
 
-nvim.command(f"edit {file}")
-
-try:
-    line = sys.argv[2]
-    nvim.command(f":{line}")
-except:
-    pass
+# If we found a socket, use it to open in existing nvim
+if socket_path:
+    try:
+        from pynvim import attach
+        nvim = attach("socket", path=socket_path)
+        nvim.command(f"edit {file}")
+        try:
+            line = sys.argv[2]
+            nvim.command(f":{line}")
+        except:
+            pass
+    except Exception as e:
+        # If connection fails, fall back to opening new nvim
+        print(f"Warning: Could not connect to nvim socket: {e}", file=sys.stderr)
+        subprocess.run(["nvim", file])
+else:
+    # No socket found, open new nvim instance
+    subprocess.run(["nvim", file])
--- nvim-pwd.py
+++ nvim-pwd.py
@@ -1,17 +1,31 @@
-#! /usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.10"
+# dependencies = ["pynvim"]
+# ///
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-#
-
-"""
-
-"""
 
 import sys
-
+import os
+import glob
 from pynvim import attach
 
-nvim = attach("socket", path="/tmp/nvim")
+# Try to get socket from NVIM environment variable first
+socket_path = os.environ.get('NVIM')
+
+# If not set, try to find the most recent nvim socket
+if not socket_path:
+    # Find all nvim sockets (both /tmp/nvim.*.sock and /tmp/nvim*/0)
+    sockets = glob.glob('/tmp/nvim.*.sock') + glob.glob('/tmp/nvim*/0')
+    if sockets:
+        # Use the most recently modified socket
+        socket_path = max(sockets, key=os.path.getmtime)
+    else:
+        print("Error: No running nvim instance found", file=sys.stderr)
+        sys.exit(1)
+
+nvim = attach("socket", path=socket_path)
 
 buffer = nvim.current.buffer
 
